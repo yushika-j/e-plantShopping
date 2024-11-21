af9daba3eb0d8adc62af2543162390ac4f6b580d
Yushika Jhundoo <98854274+yushika-j@users.noreply.github.com>
2024-11-21 10:57:51 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'; // Importing global styles
+import { Provider } from 'react-redux'; // Importing Provider for Redux
+import store from './store.js'; // Importing the Redux store
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
+    <Provider store={store}> {/* Wrapping App with Redux Provider */}
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
