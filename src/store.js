27bad55e6364dc7b880cc3bca6c932212bf2f7bb
Yushika Jhundoo <98854274+yushika-j@users.noreply.github.com>
2024-11-21 10:56:23 -0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
+// Importing Necessary Functions and Files
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
 
+// Configuring the Store
 const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: cartReducer, // Manages the state slice related to the shopping cart
   },
 });
 
+// Exporting the Store
 export default store;
